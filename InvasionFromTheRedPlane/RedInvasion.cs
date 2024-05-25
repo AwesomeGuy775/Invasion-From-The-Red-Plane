df477998b7b4a76955773c875c2898cd93c8ae6a
AwesomeGuy775 <matthewloyall@gmail.com>
2024-05-24 21:30:46 -0400

    Set up template

---
+++ InvasionFromTheRedPlane/RedInvasion.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace InvasionFromTheRedPlane
+{
+    [BepInPlugin("com.AwesomeGuy775.Invasion-From-The-Red-Plane", "Invasion From The Red Plane", "0.0.0")]
+    [BepInProcess("Risk of Rain 2.exe")]
+    public class RedInvasion : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ InvasionFromTheRedPlane/obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace InvasionFromTheRedPlane
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "InvasionFromTheRedPlane";
+        public const string PLUGIN_NAME = "InvasionFromTheRedPlane";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
