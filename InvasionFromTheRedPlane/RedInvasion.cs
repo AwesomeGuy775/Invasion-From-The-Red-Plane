1fcc378b16de1825cead9085eebb9f8bcac1c908
AwesomeGuy775 <matthewloyall@gmail.com>
2024-05-26 13:03:17 -0400

    Added R2API libraries

--- InvasionFromTheRedPlane/RedInvasion.cs
+++ InvasionFromTheRedPlane/RedInvasion.cs
@@ -1,9 +1,15 @@
 ﻿using BepInEx;
+using R2API;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace InvasionFromTheRedPlane
 {
     [BepInPlugin("com.AwesomeGuy775.Invasion-From-The-Red-Plane", "Invasion From The Red Plane", "0.0.0")]
     [BepInProcess("Risk of Rain 2.exe")]
+    [BepInDependency(ItemAPI.PluginGUID)]
+    [BepInDependency(LanguageAPI.PluginGUID)]
     public class RedInvasion : BaseUnityPlugin
     {
         private void Awake()
